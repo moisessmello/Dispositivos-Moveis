fcdeeca583d92479640744a3406cd92f6566aac0
moisessmello <moisesmelo70@gmail.com>
2025-03-07 20:32:20 -0300

    Continua revisão

--- 03 - Revisão Javascript/revisao.js
+++ 03 - Revisão Javascript/revisao.js
@@ -82,3 +82,74 @@ if(idade >= 18){
 // Operador ternário
 const checkmaiorDeIdade = idade >= 18 ? "Maior de idade" : "Menor de idade";
 console.log(checkmaiorDeIdade)
+
+// Arrays
+
+let frutas = ["Maçã", "Banana", "Uva"]
+console.log(frutas [0])
+console.log(frutas [1])
+console.log(frutas [5])
+console.log(frutas.length)
+
+frutas.push("Laranja")
+
+frutas.forEach((fruta) => {
+    console.log(fruta)
+})
+
+frutas.pop()
+console.log(frutas)
+
+// Objetos
+let pessoa = {
+    nome: "Moises",
+    idade: 33,
+    altura: 170,
+    peso: 90
+}
+//Adiciona um novo atributo (propriedade) ao objeto
+pessoa.email = "moises@ig.com"
+
+console.log(pessoa)
+
+// remove um atributo
+delete pessoa.email;
+console.log(pessoa)
+
+//Desestruturar o objeto pessoa
+let { altura, peso } = pessoa
+console.log(altura)
+console.log(peso)
+
+//Propagação de objetos
+let endereco = {
+    cidade: "Águas Lindas",
+    uf:"GO"
+}
+
+let pessoaCompleto = {...pessoa, ...endereco}
+console.log(pessoaCompleto)
+
+// Função
+function somar(numA, numB) {
+    return numA + numB
+}
+console.log(somar(2,2))
+console.log(somar(10,2))
+console.log(somar(5,5))
+
+function saudacao(nome){
+    console.log("Olá " + nome)
+}
+saudacao("Moises")
+
+// Função Anônima
+let subtrair = function (numA, numB) {
+    return numA - numB
+}
+
+console.log(subtrair(4,2))
+
+// Arrow function
+let multiplicar = (numA, numB) => numA * numB
+console.log(multiplicar(2, 10))
