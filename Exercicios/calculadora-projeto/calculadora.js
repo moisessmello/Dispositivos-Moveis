2faaaaae3ac9c5ce433cd8aa845cc53621b7b6ab
moisessmello <moisesmelo70@gmail.com>
2025-03-11 21:29:07 -0300

    Exercicio calculadora

---
+++ Exercicios/calculadora-projeto/calculadora.js
@@ -0,0 +1,18 @@
+export function soma(a, b) {
+    return a + b;
+  }
+  
+  export function subtracao(a, b) {
+    return a - b;
+  }
+  
+  export function multiplicacao(a, b) {
+    return a * b;
+  }
+  
+  export function divisao(a, b) {
+    if (b === 0) {
+      return 'Não é possível dividir por zero';
+    }
+    return a / b;
+  }
