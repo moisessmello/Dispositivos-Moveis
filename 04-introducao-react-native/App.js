97073929435d0e616fa381decb1343d0e054f986
moisessmello <moisesmelo70@gmail.com>
2025-03-18 20:37:10 -0300

    Continuação

--- 04-introducao-react-native/App.js
+++ 04-introducao-react-native/App.js
@@ -1,6 +1,6 @@
 //import
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 // Função que define o componente
 // Retornar o que vqai ser renderizado na tela (Template feito com JSX)
@@ -12,12 +12,64 @@ function alerta() {
   alert("Clicou no botão")
 }
 
+// retorno desta função do template de como vai ser
 //retorno com o JSX
   return (
+// Scolviw permite que o conteudo vá até  até depois da barra de rolagem
+// não pode ser usado sozinho, tem que ter uma viw dentro
+// ele só envolve o conteudo
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Projeto do Moises Melo</Text>
-      <StatusBar style="auto" />
+      
+      {/* <StatusBar style="auto" /> */}
+      <Text style={{ fontSize: 50, fontStyle: 'italic'}}>Projeto do Moises Melo</Text>
+      <Text style={styles.textGrande}>Bora tomar chá</Text>
+      
+      <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1QNd4BwasyZ3UWZkIF2x5WXfTRXiiYP4rjQ&s"
+        }}
 
+        style={{
+          height: 400,
+          width: 400,
+        }}
+      
+      />
+      <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1QNd4BwasyZ3UWZkIF2x5WXfTRXiiYP4rjQ&s"
+        }}
+
+        style={{
+          height: 400,
+          width: 400,
+        }}
+      
+      />
+      <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1QNd4BwasyZ3UWZkIF2x5WXfTRXiiYP4rjQ&s"
+        }}
+
+        style={{
+          height: 400,
+          width: 400,
+        }}
+      
+      />
+      <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1QNd4BwasyZ3UWZkIF2x5WXfTRXiiYP4rjQ&s"
+        }}
+
+
+        style={{
+          height: 400,
+          width: 400,
+        }}
+      
+      />
       <Image
         source={{
           uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1QNd4BwasyZ3UWZkIF2x5WXfTRXiiYP4rjQ&s"
@@ -34,6 +86,7 @@ function alerta() {
 
       <Button title='Clicar' onPress={alerta}></Button>
     </View>
+    </ScrollView>
   );
 }
 
@@ -45,4 +98,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  textGrande: {
+    fontSize: 40,
+    fontWeight: 900
+  },
+  spacer:{
+    height: 30,
+  }
+
 });
