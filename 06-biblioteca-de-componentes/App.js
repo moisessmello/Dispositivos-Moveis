8399dadac27ecb717868345a4d67eb2efae0bddd
moisessmello <moisesmelo70@gmail.com>
2025-04-01 21:49:29 -0300

    Flat List

--- 06-biblioteca-de-componentes/App.js
+++ 06-biblioteca-de-componentes/App.js
@@ -1,44 +1,70 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { PaperProvider, Card, Title, Paragraph, Divider, Button } from 'react-native-paper';
 
 
 export default function App() {
+
+  const lista = [
+    {
+      titulo: 'Card1',
+      descricao: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      imagem: 'https://i.pinimg.com/474x/9a/a7/c9/9aa7c9656614abf9bbbcc8425c878dca.jpg'
+    },
+    {
+      titulo: 'Card2',
+      descricao: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      imagem: 'https://i.pinimg.com/474x/9a/a7/c9/9aa7c9656614abf9bbbcc8425c878dca.jpg'
+    },
+    {
+      titulo: 'Card3',
+      descricao: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      imagem: 'https://i.pinimg.com/474x/9a/a7/c9/9aa7c9656614abf9bbbcc8425c878dca.jpg'
+    },
+    {
+      titulo: 'Card4',
+      descricao: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      imagem: 'https://i.pinimg.com/474x/9a/a7/c9/9aa7c9656614abf9bbbcc8425c878dca.jpg'
+    }
+  ]
   return (
+
+    
+
     <PaperProvider>
 
       <View style={styles.container}>
         <Text></Text>
         <StatusBar style="auto" />
 
-        <Button mode='contained' onPress={() => alert("Clicou")}>Clique Aqui</Button>
-        <Button icon="camera" mode="contained-tonal" >Clique aqui</Button>
-        <Button mode='contained-total' >Clique Aqui</Button>
-        <Button mode='elevated' >Clique Aqui</Button>
-        <Button mode='outlined' >Clique Aqui</Button>
-        <Button mode='text' >Clique Aqui</Button>
+ 
+        <FlatList
+        horizontal
+          data={lista}
+          renderItem={({ item }) => (
+            <Card>
+              <Card.Content>
+                <Title>{item.titulo}</Title>
+                <Paragraph>{item.descricao}</Paragraph>
+                <Card.Cover source={{uri: item.imagem}}/>
+              </Card.Content>
+            </Card>
+          )} />
+
+        <FlatList 
+          data={lista}
+          renderItem={({ item }) => (
+            <Card>
+              <Card.Content>
+                <Title>{item.titulo}</Title>
+                <Paragraph>{item.descricao}</Paragraph>
+                <Card.Cover source={{uri: item.imagem}}/>
+              </Card.Content>
+            </Card>
+          )} />
 
 
-        <Text>Uva</Text>
-        <Divider style={{width: '80%'}} />
-        <Text>Pera</Text>
-        <Divider style={{width: '80%'}} />
-        <Text>Maçã</Text>
-        <Divider style={{width: '80%'}} />
-        <Text>Banana</Text>
-        <Divider style={{width: '80%'}} />
-
-        <Text variant='titleLarge' >Um texto qualquer</Text>
-        
-        <Card>
-          <Card.Content>
-            <Title>Titulo do card</Title>
-            <Paragraph>Um parágrafo qualquer, Um parágrafo qualquer, Um parágrafo qualquer,
-            Um parágrafo qualquer,</Paragraph>
-          </Card.Content>
-          <Card.Cover source={{}}/>
-        </Card>
 
 
       </View>
@@ -57,3 +83,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
