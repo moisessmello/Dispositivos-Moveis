272068c7aa0c055a6a6325448298d104aaaff693
moisessmello <moisesmelo70@gmail.com>
2025-05-13 21:30:03 -0300

    utilizando useState

---
+++ 11-hooks/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import NomeNumero from './components/NomeNumero';
+import Controle from './components/Controle';
+import NumeroAleatorio from './components/NumeroAleatorio';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <NumeroAleatorio />
+
+      <NomeNumero />
+
+      <Controle />
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
