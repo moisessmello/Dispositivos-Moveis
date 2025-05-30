b536c8e7d5feae5959b83d660afb76348bc62e59
moisessmello <moisesmelo70@gmail.com>
2025-05-30 20:26:43 -0300

    same stuff

--- 13-crud/src/alunos/AlunoForm.jsx
+++ 13-crud/src/alunos/AlunoForm.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Button, Text, TextInput } from 'react-native-paper'
 import { TextInputMask } from 'react-native-masked-text'
+import AlunoService from './AlunoService'
 
-export default function AlunoForm() {
+export default function AlunoForm({ navigation, route}) {
 
   const [nome, setNome] = useState("")
   const [cpf, setCpf] = useState("")
@@ -11,7 +12,7 @@ export default function AlunoForm() {
   const [telefone, setTelefone] = useState("")
   const [dataNascimento, setDataNascimento] = useState("")
 
-  function salvar() {
+  async function salvar() {
     let aluno = {
       nome,
       cpf,
@@ -22,10 +23,17 @@ export default function AlunoForm() {
 
     if (!aluno.nome || !aluno.cpf || !aluno.email || !aluno.dataNascimento || !aluno.telefone) {
       alert('Preencha todos os campos!')
-    } else {
-      // gravo o aluno
+      return
+    } 
 
-    }
+      await AlunoService.salvar(aluno)
+      alert("Aluno cadastrado com sucesso!!!")
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'AlunoLista' }]
+      })
+
+
   }
 
 
--- 13-crud/src/alunos/AlunoLista.jsx
+++ 13-crud/src/alunos/AlunoLista.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FlatList, StyleSheet, View } from 'react-native'
 import { Button, Card, Text } from 'react-native-paper'
+import AlunoService from './AlunoService'
 
 export default function AlunoLista({ navigation, route }) {
 
-  const [alunos, setAlunos] = useState([
-    {
-      id: '1',
-      nome: "Teste",
-      cpf: "0010010101",
-      email: "teste@t.com",
-      dataNascimento: "02/02/2000",
-      telefone: "(61)90000-0001"
-    },
-    {
-      id: '2',
-      nome: "Teste",
-      cpf: "0010010101",
-      email: "teste@t.com",
-      dataNascimento: "02/02/2000",
-      telefone: "(61)90000-0001"
-    }
-  ])
+  const [alunos, setAlunos] = useState([ ])
+
+  useEffect(() => {
+    buscarAlunos()
+  }, [])
+
+  async function buscarAlunos() {
+    const listaAlunos = await AlunoService.listar()
+    setAlunos(listaAlunos)
+    
+  }
+
+  async function removerAluno(id) {
+    await AlunoService.remover(id)
+    alert('Aluno excluido com sucesso!!!')
+    buscarAlunos()
+    
+  }
 
 
   return (
@@ -46,8 +47,8 @@ export default function AlunoLista({ navigation, route }) {
               <Text>Email: {item.email}</Text>
             </Card.Content>
             <Card.Actions>
-              <Button>Editar</Button>
-              <Button>Excluir</Button>
+              <Button icon='pencil'> </Button>
+              <Button icon='delete' onPress={() => removerAluno(item.id)}> </Button>
             </Card.Actions>
           </Card>
         )}
